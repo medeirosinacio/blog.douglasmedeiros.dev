05c4eb80efd36eac5ad6a3518c61812f541699b2
Douglas Medeiros <douglas.medeiros@picpay.com>
2024-11-21 11:28:45 -0300

    release 0.1.5

--- assets/scripts/default.js
+++ assets/scripts/default.js
@@ -1,18 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
 
 });
-
-document.querySelector('.switch').addEventListener('click', () => {
-    const elements = document.querySelectorAll('.mode-dark');
-    elements.forEach(function (element) {
-        const classes = element.className.split(' ');
-        classes.forEach(function (className, index) {
-            if (className.includes('mode-light')) {
-                classes[index] = className.replace('mode-light', 'mode-dark');
-            } else if (className.includes('mode-dark')) {
-                classes[index] = className.replace('mode-dark', 'mode-light');
-            }
-        });
-        element.className = classes.join(' ');
-    });
-});
