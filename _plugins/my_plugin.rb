6c2308d918d5ad0722ffb30e2917d22f87d62c5e
Douglas Medeiros <douglas.medeiros@picpay.com>
2025-06-25 10:05:05 -0300

    teste filtro
    Signed-off-by: Douglas Medeiros <douglas.medeiros@picpay.com>

---
+++ _plugins/my_plugin.rb
@@ -0,0 +1,13 @@
+require 'nokogiri'
+
+module MyPlugin
+	def responsive_img_src(html_source)
+	  puts "Filtro chamado"
+	  doc = Nokogiri::HTML::Document.parse('<html></html>', nil, 'UTF-8')
+	  fragment = Nokogiri::HTML::DocumentFragment.new(doc, html_source)
+	  fragment.css('img').each { |img| img['data-src'] = img['src'] }
+	  return fragment.to_html
+	end
+end
+
+Liquid::Template.register_filter(MyPlugin)
